076812c5277d10cb9f1a2ac06bb885ef0985cda7
Ryan Faircloth <ryan@dss-i.com>
2023-05-22 10:54:13 -0400

    fix: missing provider

--- versions.tf
+++ versions.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     azuread = {
-      source = "hashicorp/azuread"
+      source  = "hashicorp/azuread"
       version = "2.39.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.20.0"
     }
   }
 }
